afe2948b7534e6f757eb41e803aaebc823a11692
KyleCheng <kyle_cheng_3@sfu.ca>
2020-07-01 18:12:03 -0700

    add start button animation

--- Bad Intention/Assets/Scripts/ScriptCharacterSelection.cs
+++ Bad Intention/Assets/Scripts/ScriptCharacterSelection.cs
@@ -2,12 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ScriptCharacterSelection : MonoBehaviour
 {
+
+	Camera MainCamera;
+	public Camera Camera2;
+
+	void Start()
+	{
+		MainCamera = Camera.main;
+		MainCamera.enabled = true;
+		Camera2.enabled = false;
+	}
+
 	public void ButtonMoveScene(string level)
     {
     	SceneManager.LoadScene(level);
+    }
+
+    public void ButtonMoveCamera()
+    {
+    	if(MainCamera.enabled)
+    	{
+    		Camera2.enabled = true;
+    		MainCamera.enabled = false;
+    	}
+    	else
+    	{
+    		Camera2.enabled = false;
+    		MainCamera.enabled = true;
+    	}
     }
 
     void Update()
