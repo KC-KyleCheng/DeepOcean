68990cf223beea3ca2f4d30b291e01f5270ad1d4
KyleCheng0217 <gcobs156410@hotmail.com>
2020-10-23 18:42:33 -0700

    camera now only moves when user finger moves

--- Bad Intention/Assets/Scripts/Cameras.cs
+++ Bad Intention/Assets/Scripts/Cameras.cs
@@ -7,9 +7,11 @@ using UnityEngine.UI;
 public class Cameras : MonoBehaviour
 {
     private Vector3 dragOrigin;
-    private Vector3 endOrigin;
-    private Vector3 pos;
+    private Vector3 dragEnd;
+    private Vector3 distance;
     private Vector3 cameraPosition;
+    private Vector3 StartingPos;
+    private Vector3 EndingPos;
 
     private Vector3 character1 = new Vector3(124, 355, -10);
     private Vector3 character2 = new Vector3(370, 353, -10);
@@ -30,21 +32,24 @@ public class Cameras : MonoBehaviour
 
             if(touch.phase == TouchPhase.Moved)
             {
-            	endOrigin = touch.position;
-            	cameraPosition = this.transform.position;
+            	dragEnd = touch.position;       	                
 
-            	float lerpValueX = Mathf.Lerp(dragOrigin.x, endOrigin.x, (Time.time-2.0f)*0.2f);
-    			float lerpValueY = Mathf.Lerp(dragOrigin.y, endOrigin.y, (Time.time-2.0f)*0.2f);
-   				transform.position = new Vector3(lerpValueX, lerpValueY, -10);
+        		distance = dragOrigin - dragEnd;
 
-   				/*if(cameraPosition.x >= 110 && cameraPosition.x <= 839) //within the max horizontal boundary
-            	{
-            		pos = Camera.main.ScreenToViewportPoint(dragOrigin - endOrigin);
-            		endOrigin.z = -10;
-    				transform.Translate(endOrigin, Space.World); 
-    				transform.position = Vector3.MoveTowards(this.transform.position, endOrigin, 30);
-    
-            	}*/
+                EndingPos.x = StartingPos.x + distance.x/2;
+                EndingPos.y = StartingPos.y + distance.y/2;
+
+                EndingPos.x = Mathf.Min(EndingPos.x, 825);
+                EndingPos.x = Mathf.Max(EndingPos.x, 124);
+                EndingPos.y = Mathf.Min(EndingPos.y, 380);
+                EndingPos.y = Mathf.Max(EndingPos.y, 300);
+
+                //Debug.Log("EndingPos: " + EndingPos.x + "x" + EndingPos.y);
+
+                float lerpValueX = Mathf.Lerp(StartingPos.x, EndingPos.x, (Time.time-2.0f)*0.2f);
+                float lerpValueY = Mathf.Lerp(StartingPos.y, EndingPos.y, (Time.time-2.0f)*0.2f);
+
+                transform.position = new Vector3(lerpValueX, lerpValueY, -10);   
 
             }
             
@@ -60,26 +65,31 @@ public class Cameras : MonoBehaviour
         	if(Mathf.Abs(cameraPosition.x - character1.x) < 125 || cameraPosition.x < character1.x)
         	{
         		transform.position = Vector3.MoveTowards(this.transform.position, character1, 30);
+                StartingPos = character1;
         	}
 
         	else if(Mathf.Abs(cameraPosition.x - character2.x) < 125)
         	{
         		transform.position = Vector3.MoveTowards(this.transform.position, character2, 30);
+                StartingPos = character2;
         	}
 
         	else if(Mathf.Abs(cameraPosition.x - character3.x) < 125)
         	{
         		transform.position = Vector3.MoveTowards(this.transform.position, character3, 30);
+                StartingPos = character3;
         	}
 
         	else if(Mathf.Abs(cameraPosition.x - character4.x) < 125 || cameraPosition.x > character4.x)
         	{
         		transform.position = Vector3.MoveTowards(this.transform.position, character4, 30);
+                StartingPos = character4;
         	}
 
         	else
         	{
         		transform.position = Vector3.MoveTowards(this.transform.position, character1, 30);
+                StartingPos = character1;
         	}
         		
         }
